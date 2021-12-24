803908e373b7a0fe094e05ca070e47314e64b339
HiveTrending <hive_up@protonmail.com>
2021-12-24 12:33:15 -0800

    Add more stats

--- dluxmonitor/dluxmonitor.js
+++ dluxmonitor/dluxmonitor.js
@@ -25,21 +25,38 @@ stats_promise = axios({
 
 Promise.all([coin_promise, runners_promise, queue_promise, stats_promise])
 .then((values) => {
-    console.log(values[0].data)
-    console.log(values[1].data)
-    console.log(values[2].data)
-    console.log(values[3].data)
+    let [coin, runners, queue, stats] = values
+    console.log(coin.data)
+    console.log(runners.data)
+    console.log(queue.data)
+    console.log(stats.data)
 
-    let runners = values[1].data.runners
-    let queue = values[2].data.queue
+    coin = coin.data
+    let coin_info = coin.info
+    runners = runners.data.runners
+    queue = queue.data.queue
+    stats = stats.data.result
 
-    // populate check string
-    document.querySelector('span#check').innerHTML = values[0].data.check
+    let stats_rows = {}
+    stats_rows['Total Supply'] = (stats.tokenSupply / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in NFTs'] = (coin_info.in_NFTS / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in Auctions'] = (coin_info.in_auctions / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in Contracts'] = (coin_info.in_contracts / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in Dividends'] = (coin_info.in_dividends / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in Market'] = (coin_info.in_market / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in Governance'] = (coin_info.locked_gov / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Locked in PowerUps'] = (coin_info.locked_pow / 1000).toLocaleString() + ' DLUX'
+    stats_rows['Liquid Supply'] = (coin_info.liquid_supply / 1000).toLocaleString() + ' DLUX'
+    
 
-    // populate gov_threshhold string
-    let gov_threshhold = parseInt(values[3].data.result.gov_threshhold) / 1000
-    gov_threshhold = `${gov_threshhold} DLUX`
-    document.querySelector('span#gov_threshhold').innerHTML = gov_threshhold 
+    stats_rows['Governance Threshold'] = (parseInt(stats.gov_threshhold) / 1000).toLocaleString() + ' DLUXG'
+    stats_rows['Blocks Behind'] = coin.behind + ' blocks'
+
+    let statsTable = ''
+    for (attribute in stats_rows) {
+      statsTable += `<tr><td>${attribute}</td><td>${stats_rows[attribute]}</td></tr>`
+    }
+    document.querySelector('table#stats').innerHTML += statsTable
 
     // populate runners table
     table_markup = '<thead><th>Name</th><th>Runner?</th><th>DLUXG</th><th>API</th></thead>'
