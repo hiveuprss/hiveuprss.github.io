0ed1b72c3a444e2e59dd36e5015db80db897d1e4
HiveTrending <62359919+hiveuprss@users.noreply.github.com>
2024-10-23 14:55:50 -0700

    fix dupe rows

--- accountstream/index.js
+++ accountstream/index.js
@@ -1,12 +1,5 @@
 // Created by peakd.com/@hivetrending
-
 hive.api.setOptions({ url: "https://api.deathwing.me/" });
-
-var speed = 3000;
-var width = 800,
-  height = 700;
-
-var color = "gray";
 
 function clamp(num, min, max) {
   return num <= min ? min : num >= max ? max : num;
@@ -47,7 +40,7 @@ document.querySelector("button#play").onclick = (e) => {
 };
 
 document.querySelector("button#fastforward").onclick = (e) => {
-  var minSpeed = 3.0;
+  var minSpeed = 1.0;
   var maxSpeed = 10.0;
   var speedIncrement = 1.0;
 
@@ -66,7 +59,7 @@ document.querySelector("button#fastforward").onclick = (e) => {
 
 function getSpeedSetting() {
   if (!document.querySelector("button#speedgauge").data) {
-    document.querySelector("button#speedgauge").data = "3.0";
+    document.querySelector("button#speedgauge").data = "1.0";
   }
 
   var currentSpeed = parseFloat(
@@ -88,13 +81,10 @@ function getLatestBlocknum() {
     var blockNum = parseInt(result.head_block_number) - 2_000;
     document.querySelector("#blockNum").innerText = `${blockNum}`;
     document.querySelector("#blockNum").data = `${blockNum}`;
-    runLoop();
   });
 }
 
 function runLoop() {
-
-  //startSimulation()
   if (document.querySelector("button#pause").hidden == true) {
     return;
   }
@@ -105,22 +95,21 @@ function runLoop() {
     return;
   }
 
-  //console.log(blockNum)
-  const blockRangeSize = 10;
+  const blockRangeSize = 20;
+
+  // console.log(`{ starting_block_num: ${blockNum}, count: ${blockRangeSize} }`)
 
   hive.api.call(
     "block_api.get_block_range",
-    { starting_block_num: blockNum, count:blockRangeSize  },
+    { starting_block_num: blockNum, count: blockRangeSize },
     async function (err, result) {
-
-      //console.log(err, result);
-      //console.log(blockNum)
       if (err) {
         console.log(err);
         return;
       }
 
-      if (result.blocks.length < blockRangeSize) {
+      if (result.blocks.length == 0) {
+        return;
       }
 
       for (const block of result.blocks) {
@@ -129,37 +118,35 @@ function runLoop() {
           return;
         }
 
-        block.transactions = block.transactions.filter((tx) => {
-
-          var opname = tx.operations[0].type;
-
-          // filter for account creation only
-          if (["account_create_operation","create_claimed_account_operation"].includes(opname)) {
-            if (!filterByCreator) {
-              return true;
-            } else if (tx.operations[0].value.creator === filterByCreator) {
-              return true;
-            }
-            return false;
-          }
-          return false;
-        });
-
-
         document.querySelectorAll("div.transfer").forEach((node) => {
           node.className = "transfer gray";
         });
 
-        block.transactions.forEach((tx) => {
+        for (const tx of block.transactions) {
           var op = tx.operations[0];
           var opname = tx.operations[0].type;
 
-          var color = "green";
-          color = "lightgreen";
+          if (
+            ![
+              "account_create_operation",
+              "create_claimed_account_operation",
+            ].includes(opname)
+          ) {
+            continue;
+          }
+
+          if (
+            filterByCreator &&
+            tx.operations[0].value.creator !== filterByCreator
+          ) {
+            continue;
+          }
+
+          const color = "lightgreen";
 
           op["to"] = op.value.new_account_name;
           op["from"] = op.value.creator;
-          op["amount"] = op.value.fee ? op.value.fee.amount/1000 : 0.000;
+          op["amount"] = op.value.fee ? op.value.fee.amount / 1000 : 0.0;
 
           const timestamp = new Date(block.timestamp).toLocaleString();
 
@@ -167,24 +154,15 @@ function runLoop() {
           document.querySelector("div#content").innerHTML =
             `<div class="transfer ${color}">[${timestamp} UTC] <span class="bold"><a href="https://peakd.com/@${op.from}">@${op.from}</a></span> created <span class="bold"><a href="https://peakd.com/@${op.to}">@${op.to}</a></span> ( ${op["amount"]} $Hive fee )</div>` +
             currentHTML;
-        });
+
+          console.log('block_id', block.block_id);
+        }
       }
 
-      const lastBlock = result.blocks[result.blocks.length - 1];
-      if (!lastBlock || !lastBlock.transactions) {
-        return;
-      }
-
-      // if we succeeded so far, advance to next block
-      console.log(parseInt(blockNum) + result.blocks.length);
-      if (document.querySelector("#blockNum").data == `${parseInt(blockNum)}`) {
-        document.querySelector("#blockNum").data = `${parseInt(blockNum) + result.blocks.length}`;
-        document.querySelector("#blockNum").innerText = `${blockNum}`;
-      }
-
-      if (document.querySelector("button#pause").hidden == true) {
-        return;
-      }
+      // if we succeeded so far with this block, advance to next block range
+      const newBlockNum = parseInt(blockNum) + result.blocks.length;
+      document.querySelector("#blockNum").data = `${newBlockNum}`;
+      document.querySelector("#blockNum").innerText = `${newBlockNum}`;
     }
   );
 }
@@ -192,15 +170,13 @@ function runLoop() {
 // initialize, read params
 var urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has("block")) {
-  var blockNum = urlParams.get("block");
-  var blockNum = parseInt(blockNum);
+  var blockNum = parseInt(urlParams.get("block"));
 
   if (isNaN(blockNum) || blockNum < 0) {
     getLatestBlocknum();
   } else {
     document.querySelector("#blockNum").innerText = `${blockNum}`;
     document.querySelector("#blockNum").data = `${blockNum}`;
-    runLoop();
   }
 } else {
   getLatestBlocknum();
@@ -222,4 +198,4 @@ function runtimeAdjustSpeed() {
   }, currentSpeed);
 }
 
-runtimeAdjustSpeed();
+(runtimeAdjustSpeed());
